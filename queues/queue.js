3153d65579efdbdffc99b6a3a965a9c5e2e7a4df
Minh Chau <xiu.chau@gmail.com>
2017-07-03 17:00:47 -0700

    simple queue with linked list

---
+++ queues/queue.js
@@ -0,0 +1,22 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  enqueue() {
+    // ...push
+  }
+
+  dequeue() {
+    // ...shift
+  }
+}
